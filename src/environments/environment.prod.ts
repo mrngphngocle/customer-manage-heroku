3497d3e575d1d351f557381b66c10822f39d3e83
thiendi <mrngphngocle@gmail.com>
2021-07-06 00:08:05 +0700

    deploy env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFwoYVIaBktPGgj3W9uqeCMARpSUV8ElA",
+    authDomain: "customermanage-9d130.firebaseapp.com",
+    databaseURL: "https://customermanage-9d130-default-rtdb.firebaseio.com",
+    projectId: "customermanage-9d130",
+    storageBucket: "customermanage-9d130.appspot.com",
+    messagingSenderId: "1068582962175",
+    appId: "1:1068582962175:web:2cf3ed7234ad2afc191dca"
+  }
 };
