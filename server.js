af5a45aab0a87c513819f01760cfa8e57ac234e5
thiendi <mrngphngocle@gmail.com>
2021-07-06 11:02:08 +0700

    deploy env json lock ok

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/customer-manage'));
+app.use(express.static('./dist/client'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/customer-manage/'}),
+    res.sendFile('index.html', {root: 'dist/client/'}),
 );
 
 // Start the app by listening on the default Heroku port
